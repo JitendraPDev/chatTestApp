79301f7bb8ebf64cebdf6eb6baf6bc74a5cdbfb6
JitendraPDev <89909023+JitendraPDev@users.noreply.github.com>
2021-12-21 18:07:42 +0530

    inital commit

---
+++ sdkIndore/sdkIndore.h
@@ -0,0 +1,18 @@
+//
+//  sdkIndore.h
+//  sdkIndore
+//
+//  Created by Apple on 21/12/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for sdkIndore.
+FOUNDATION_EXPORT double sdkIndoreVersionNumber;
+
+//! Project version string for sdkIndore.
+FOUNDATION_EXPORT const unsigned char sdkIndoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <sdkIndore/PublicHeader.h>
+
+
